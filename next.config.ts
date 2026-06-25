2ea1a520741d313f237a38bde122d5b892c79e9f
Z User <z@container>
2026-06-25 03:40:20 +0000

    3e2470b7-1c16-4fed-9bd4-8cb2dc707f79

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
-  reactStrictMode: false,
 };
 
 export default nextConfig;
